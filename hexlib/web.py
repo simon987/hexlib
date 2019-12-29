37257f11ebb6262fb39283fa72cd0f20cade9d40
simon987 <me@simon987.net>
2019-12-29 13:25:43 -0500

    save and load cookie

--- hexlib/web.py
+++ hexlib/web.py
@@ -1,6 +1,10 @@
 from http.cookiejar import Cookie
+import re
 
 from dateutil.parser import parse
+import pickle
+
+from requests.cookies import RequestsCookieJar
 
 
 def cookie_from_string(text: str, domain: str) -> Cookie:
@@ -33,3 +37,25 @@ def cookie_from_string(text: str, domain: str) -> Cookie:
         rest=None,
         rfc2109=False
     )
+
+
+def save_cookiejar(cj, filename):
+    with open(filename, "wb") as f:
+        f.truncate()
+        pickle.dump(cj._cookies, f)
+
+
+def load_cookiejar(filename):
+    with open(filename, "rb") as f:
+        cookies = pickle.load(f)
+        cj = RequestsCookieJar()
+        cj._cookies = cookies
+    return cj
+
+
+def cookiejar_filter(cj, pattern):
+    filtered_cj = RequestsCookieJar()
+    for c in cj:
+        if re.match(pattern, c.domain):
+            filtered_cj.set_cookie(c)
+    return filtered_cj
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hexlib",
-    version="1.4",
+    version="1.5",
     description="Misc utility methods",
     author="simon987",
     author_email="me@simon987.net",
